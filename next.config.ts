714161361956aff08ec2e8bfe6dd7062d81a885d
Celso Fabri Junior <celso.fabri@live.com>
2025-06-03 00:00:02 -0300

    chore: configure deploy GH Pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
-// next.config.js
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-};
+  output: 'export', // Isso indica que você quer exportar um site estático (sem backend)
+  images: { unoptimized: true },  // Para evitar erro com imagens no modo estático
+  // Se você for publicar no GitHub Pages e seu repo for usado como subpath, descomente e ajuste:  
+  // basePath: '/fashionCommerce-web',
+  // assetPrefix: '/fashionCommerce-web/',
+}
 
 module.exports = nextConfig;
