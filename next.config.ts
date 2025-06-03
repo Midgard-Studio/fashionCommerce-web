640c36e51376d73ab936ff028c1a6b661514d3dd
Celso Fabri Junior <celso.fabri@live.com>
2025-06-02 23:32:43 -0300

    feat: configure build and workflow to deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
