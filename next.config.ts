0a741af175dc3c29ae21d6005499f04c2eb8fdeb
Celso Fabri Junior <celso.fabri@live.com>
2025-06-02 23:52:21 -0300

    chore: update deploy settings

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 // next.config.js
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/fashionCommerce-web",
+  assetPrefix: "/fashionCommerce-web/",
   output: 'export',
   images: {
     unoptimized: true,
