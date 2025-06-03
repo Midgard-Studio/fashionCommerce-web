a825616b538c719643928836a9502874d833f62f
Celso Fabri Junior <celso.fabri@live.com>
2025-06-03 00:13:15 -0300

    fix: update deployment setup

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // Isso indica que você quer exportar um site estático (sem backend)
-  images: { unoptimized: true },  // Para evitar erro com imagens no modo estático
-  // Se você for publicar no GitHub Pages e seu repo for usado como subpath, descomente e ajuste:  
-  // basePath: '/fashionCommerce-web',
+  output: 'export',
+  images: { unoptimized: true },
+  // basePath: '/fashionCommerce-web',    // descomente se publicar em subpasta
   // assetPrefix: '/fashionCommerce-web/',
 }
 
